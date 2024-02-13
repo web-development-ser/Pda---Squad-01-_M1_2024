861997dbf63f6d2dbe7888e26160092d82a6de56
Sabrina Marques <sabrinamarques0240@gmail.com>
2024-02-13 19:44:31 -0300

    Adição das perguntas

--- assets/data.js
+++ assets/data.js
@@ -1,19 +1,24 @@
 // Edite e adcione mais questões:
 const dataQuiz = [
     {
-        question : "Q: Questão 01",
-        alternatives : ["Ops 01", "Ops 02", "Ops 03"],
+        question : "Quem é considerado o rei dos deuses da mitologia grega?",
+        alternatives : ["Hades", "Zeus", "Apolo", "Poseidon"],
         index_correct_question : 1
     },
     {
-        question : "Q: Questão 02",
-        alternatives : ["Ops 01", "Ops 02", "Ops 03", "Ops 07"],
+        question : "Qual é o nome da deusa da sabedoria e da guerra estratégica?",
+        alternatives : ["Afrodite", "Hera", "Atena", "Deméter"],
         index_correct_question : 2
     },
     {
-        question : "Q: Questão 03",
-        alternatives : ["Ops 01", "Ops 02", "Ops 03", "Ops 04"],
+        question : "Quem é o deus do submundo na mitologia grega?",
+        alternatives : ["Apolo", "Ares", "Zeus", "Hades"],
         index_correct_question : 3
+    },
+    {
+        question : "Quem foi o herói que matou a Medusa na mitologia grega?",
+        alternatives : ["Perseu", "Teseu", "Hércules", "Jasão"],
+        index_correct_question : 0
     }
 ];
 

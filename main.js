ceb53089cbcb59b01c4726f5effdc8c59caf1f2e
Ant Sérgio <sergio-web-development@protonmail.com>
2024-02-14 19:28:54 -0300

    0005

--- main.js
+++ main.js
@@ -119,3 +119,24 @@ function next () {
 document.getElementById('next').onclick = next;
 
 // -----------------------------------------------------------------
+
+
+
+// -----------------------------------------------------------------
+const dataInfo = [
+    {posicao: 'Líder', nome : "Amadeu Fernandes da Silva Neto"},
+    {posicao: 'Comunicador', nome: "Kozikla Sanara Criri Rodrigues"},
+    {posicao: 'Gestor/a do conhecimento', nome: "Stéphanie Maria Câmdido dos Santos"},
+    {posicao: 'Colaborador/a', nome: "Antonio Sérgio Viana dos Santos"},
+    {posicao: 'Colaborador/a', nome: "Caio da Silva Sousa"},
+    {posicao: 'Colaborador/a', nome: "Sabrina Alves Marques"}
+];
+const initQuiz = () => {
+    let username = prompt("Como deseja ser chamado?");
+    document.getElementById('user').innerHTML += username;
+    let x = 0;
+    while (x < dataInfo.length) {
+        document.getElementById('iteg').innerHTML += `<p>${dataInfo[x].posicao}: <span>${dataInfo[x].nome}</span></p>`;
+        x++;
+    }
+}; initQuiz ();

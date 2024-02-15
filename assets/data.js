08b51e313ddf9055de0341ff4687068a88962955
Stéphanie Cândido <stephanie.candido42@gmail.com>
2024-02-15 15:21:40 -0300

    feat:adiciona novas questões ao quiz

--- assets/data.js
+++ assets/data.js
@@ -26,6 +26,51 @@ const dataQuiz = [
         alternatives : ["Hefesto", "Hélios", "Apolo", "Jasão"],
         index_correct_question : 1
 
+    },
+    {
+        question : "Quem construiu o labirinto do Minotauro?",
+        alternatives : ["Dédalo", "Ícaro", "Hefesto", "Atena"],
+        index_correct_question : 0
+    },
+    {
+        question : "Qual é o símbolo de Hermes?",
+        alternatives : ["Tridente", "Harpa", "Sandália com asas", "Caduceu"],
+        index_correct_question : 3
+    },
+    {
+        question : "Quem é o deus do sono na mitologia gregra",
+        alternatives : ["Hipnos", "Morfeu", "Orfeu", "Zeus"],
+        index_correct_question : 0
+    },
+    {
+        question : "Afrodite é casada com qual deus grego?",
+        alternatives : ["Hermes", "Hefesto", "Ares", "Apolo"],
+        index_correct_question : 1
+    },
+    {
+        question : "Qual o primeiro ser do universo, segundo a mitologia grega?",
+        alternatives : ["Gaia", "Urano", "Tártaro", "Caos"],
+        index_correct_question : 3
+    },
+    {
+        question : "Qual dessas criaturas não são filhos de Urano e Gaia?",
+        alternatives : ["Titãs", "Ciclopes", "Nyx", "Centímanos"],
+        index_correct_question : 2
+    },
+    {
+        question : "Qual foi o segundo trabalho de Hércules?",
+        alternatives : ["matar o leão de Nemeia", "capturar Cérbero", "matar a hidra de Lerna", "derrotar Medusa"],
+        index_correct_question : 2
+    },
+    {
+        question : "Qual o deus dos rios e correntes de água doce?",
+        alternatives : ["Poseidon", "Aqueloo", "Nereu", "Ponto"],
+        index_correct_question : 1
+    },
+    {
+        question : "Qual o símbolo da deusa grega da caça e da castidade?",
+        alternatives : ["Lua e estrelas", "Arco e flecha", "Harpa", "Faca" ],
+        index_correct_question : 1
     }
 ];
 

06da38e76192de5d0521d32fc93a9f6a7cbd8fe5
Amadeu <amadeuneto1618@gmail.com>
2024-02-16 15:01:32 -0300

    Resolvendo bugs dos níveis e botões

--- assets/data.js
+++ assets/data.js
@@ -1,39 +1,33 @@
 // Edite e adcione mais questões:
 const dataQuiz = [
     {
-        question: "Quem foi o herói que matou a Medusa na mitologia grega?",
+        question: "1. Qual o herói que matou a Medusa na mitologia grega?",
         alternatives: ["Jasão", "Teseu", "Hércules", "Perseu"],
         index_correct_question: 3
     }, 
 
     {
-        question: "Quem foi o herói que matou a Medusa na mitologia grega?",
-        alternatives: ["Jasão", "Teseu", "Hércules", "Perseu"],
-        index_correct_question: 3
-    },
-
-    {
-        question: "Quem é o deus do sol na mitologia grega?",
+        question: "2. Quem é o deus do sol na mitologia grega?",
         alternatives: ["Hefesto", "Hélios", "Apolo", "Jasão"],
         index_correct_question: 1
     },
 
     {
-        question: "Quem construiu o labirinto do Minotauro?",
+        question: "3. Quem construiu o labirinto do Minotauro?",
         alternatives: ["Ícaro", "Dédalo", "Hefesto", "Atena"],
         index_correct_question: 1
     },
 
     {
-        question: "Qual é o símbolo de Hermes?",
-        alternatives: ["Tridente", "Harpa", "Sandália com asas", "Caduceu"],
-        index_correct_question: 3
+        question: "4. Quem é o deus do sono na mitologia gregra",
+        alternatives: ["Morfeu", "Hipnos", "Orfeu", "Zeus"],
+        index_correct_question: 1
     },
 
     {
-        question: "Quem é o deus do sono na mitologia gregra",
-        alternatives: ["Morfeu", "Hipnos", "Orfeu", "Zeus"],
-        index_correct_question: 1
+        question: "5. Qual é o símbolo de Hermes?",
+        alternatives: ["Tridente", "Harpa", "Sandália com asas", "Caduceu"],
+        index_correct_question: 3
     },
 ];
 export default dataQuiz;

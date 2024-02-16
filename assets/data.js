0756b444e909b4ff236ef9c011dedb0360ed063a
sanaraskz <160195591+sanaraskz@users.noreply.github.com>
2024-02-15 23:43:52 -0300

    Atualização de questões

--- assets/data.js
+++ assets/data.js
@@ -1,20 +1,11 @@
 // Edite e adcione mais questões:
 const dataQuiz = [
-    {
-        question : "Quem é considerado o rei dos deuses da mitologia grega?",
-        alternatives : ["Hades", "Zeus", "Apolo", "Poseidon"],
-        index_correct_question : 1
-    },
-    {
-        question : "Qual é o nome da deusa da sabedoria e da guerra?",
-        alternatives : ["Afrodite", "Hera", "Atena", "Deméter"],
-        index_correct_question : 2
-    },
-    {
-        question : "Quem é o deus do submundo na mitologia grega?",
-        alternatives : ["Apolo", "Ares", "Zeus", "Hades"],
-        index_correct_question : 3
-    },
+   {
+    question : "Quem foi o herói que matou a Medusa na mitologia grega?",
+    alternatives : ["Jasão", "Teseu", "Hércules", "Perseu"],
+    index_correct_question : 3
+   }, 
+
     {
         question : "Quem foi o herói que matou a Medusa na mitologia grega?",
         alternatives : ["Jasão", "Teseu", "Hércules", "Perseu"],
@@ -45,31 +36,6 @@ const dataQuiz = [
     {
         question : "Afrodite é casada com qual deus grego?",
         alternatives : ["Hermes", "Hefesto", "Ares", "Apolo"],
-        index_correct_question : 1
-    },
-    {
-        question : "Qual o 1° ser do universo, segundo a mitologia grega?",
-        alternatives : ["Gaia", "Urano", "Tártaro", "Caos"],
-        index_correct_question : 3
-    },
-    {
-        question : "Qual dessas criaturas não são filhos de Urano e Gaia?",
-        alternatives : ["Titãs", "Ciclopes", "Nyx", "Centímanos"],
-        index_correct_question : 2
-    },
-    {
-        question : "Qual foi o segundo trabalho de Hércules?",
-        alternatives : ["Matar o leão de Nemeia", "Capturar Cérbero", "Matar a hidra de Lerna", "Derrotar Medusa"],
-        index_correct_question : 2
-    },
-    {
-        question : "Qual o deus dos rios e correntes de água doce?",
-        alternatives : ["Poseidon", "Aqueloo", "Nereu", "Ponto"],
-        index_correct_question : 1
-    },
-    {
-        question : "Qual o símbolo da deusa grega da caça e da castidade?",
-        alternatives : ["Lua e estrelas", "Arco e flecha", "Harpa", "Faca" ],
         index_correct_question : 1
     }
 ];
